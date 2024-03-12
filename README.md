73db6185eba636be04e95e44f6ff17638b7ed1a6
Stanislau Tur <stanislau.tur@gmail.com>
2024-03-12 14:40:27 +0100

    Add iterm profile

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+### Git configs
+```bash
+echo "[user]\nemail = ..." >> ~/personal.gitconfig
+echo "[user]\nemail = ..." >> ~/workspace.gitconfig
+```
+
+### Stow commands
+
 ```bash
 stow --target=$HOME --restow */
 ```
--- install.sh
+++ install.sh
@@ -7,9 +7,8 @@ fi
 
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>$HOME/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
